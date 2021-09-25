e9ad07c90dafd91ea7b28c28ce11e0b9ce0d7d23
Francisco Gindre <francisco.gindre@gmail.com>
2021-09-24 21:10:45 -0300

    [WIP] nice impl but that does not work because of private attributes of ZcashAddress

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,20 @@
 // mod zcash_address::{ZcashAddress};
 uniffi_macros::include_scaffolding!("zcash_address");
 
+use zcash_address::{ZcashAddress, ParseError, Network};
+
 #[derive(Debug, thiserror::Error)]
 pub enum AddressError {
     #[error("The received address string is invalid")]
     InvalidAddress,
+    #[error("The received address string does not appear to be a Zcash address")]
+    NotZcash,
+    #[error("The received address was matched as a UA but resulted invalid")]
+    InvalidUnifiedAddress,
 }
+
 #[derive(Debug, Clone)]
-pub enum Kind {
-    Sransparent,
-    Sapling,
-    Unified,
-}
-#[derive(Debug, Clone)]
-pub enum Network {
+pub enum NetworkType {
     Main,
     Test,
     Regtest
@@ -22,38 +23,81 @@ pub enum Network {
 
 #[derive(Debug)]
 pub struct Address {
-    kind: Kind,
-    network: Network,
+    network: NetworkType,
     string: String,
 }
 
+fn parse(
+    address_text: String
+) -> Result<Address, AddressError> {
+    ZcashAddress::try_from_encoded(&address_text.as_str())
+        .map(|zcash_address| zcash_address.to_address())
+        .map_err(|e| e.to_address_error())
+}
 
-impl Address {
-    fn parse(address_text: &str) -> Result<Self, AddressError> {
-        Err(AddressError::InvalidAddress)
+fn derive_transparent_address(
+    seed_bytes: &[u8], 
+    account: i32, 
+    index: i32
+) -> Result<Address, AddressError> {
+    Err(AddressError::InvalidAddress)
+}
+
+fn derive_sapling_address(
+    seed_bytes: &[u8],
+    account: i32,
+    index: i32
+) -> Result<Address, AddressError> {
+    Err(AddressError::InvalidAddress)       
+}
+
+fn derive_unified_address(
+    seed_bytes: &[u8],
+    account: i32,
+    index: i32
+) -> Result<Address, AddressError> {
+    Err(AddressError::InvalidAddress)       
+}
+trait ToAddress {
+    fn to_address(&self) -> Address;
+}
+
+trait ToAddressError {
+    fn to_address_error(&self) -> AddressError;
+}
+
+trait ToNetworkType {
+    fn to_network_type(&self) -> NetworkType;
+}
+
+impl ToNetworkType for Network {
+    fn to_network_type(&self) -> NetworkType {
+        match self {
+            Network::Main => NetworkType::Main,
+            Network::Test => NetworkType::Test,
+            Network::Regtest => NetworkType::Regtest,
+        }
     }
+}
 
-    fn derive_transaparent_address(
-        seed_bytes: &[u8], 
-        account: i32, 
-        index: i32
-    ) -> Result<Self, AddressError> {
-        Err(AddressError::InvalidAddress)
+impl ToAddressError for ParseError {
+    fn to_address_error(&self) -> AddressError {
+        match self {
+            ParseError::InvalidEncoding => AddressError::InvalidAddress,
+            ParseError::NotZcash => AddressError::InvalidAddress,
+            ParseError::Unified(_) => AddressError::InvalidUnifiedAddress
+        }
     }
+}
+impl ToAddress for ZcashAddress {
+    fn to_address(&self) -> Address {
+        let address = Address {
+            network: self.net.to_network_type(),
+            string: self.to_string()
+        }
 
-    fn derive_sapling_address(
-        seed_bytes: &[u8],
-        account: i32,
-        index: i32
-    ) -> Result<Self, AddressError> {
-        Err(AddressError::InvalidAddress)       
-    }
 
-    fn derive_unified_address(
-        seed_bytes: &[u8],
-        account: i32,
-        index: i32
-    ) -> Result<Self, AddressError> {
-        Err(AddressError::InvalidAddress)       
+        return address
+
     }
 }
--- tests/bindings/test_zcash_address_helper.swift
+++ tests/bindings/test_zcash_address_helper.swift
@@ -1,42 +1,13 @@
 import zcash_address
 
+extension zcash_address.Address: Equatable {}
 
-// extension Network: Equatable {
-//     static func ==(lhs: Network, rhs: Network) -> Bool {
-//         switch lhs {
-//         case .main:
-//             switch rhs {
-//             case .main:
-//                 return true
-//             default:
-//                 return false
-//             }
-//         case .test: 
-//         switch rhs {
-//             case .test:
-//                 return true
-//             default:
-//                 return false
-//             }
-//         case .regtest:
-//             switch rhs {
-//             case .regtest:
-//                 return true
-//             default:
-//                 return false
-//             }    
-//         }
-//     }
-// }
-
-// let helper = ZcashAddressHelper()
-
-// do {
-//     let testSaplingAddress = "ztestsapling1ysrf4uq52n5hhj0vzxpcfneszlk8flalh3ajdwsyucnpc0fjktp9afzcclnxdrnzfl7w7wyx3kz"
-//     let parsedAddress  = try helper.parse(testSaplingAddress)
-//     // assert(parsedAddress.network == .test)
-//     assert(true)
-// } catch {
-//     fatalError("Invalid Address when it should have been valid")
-// }
+do {
+    let testSaplingAddress = "ztestsapling1ysrf4uq52n5hhj0vzxpcfneszlk8flalh3ajdwsyucnpc0fjktp9afzcclnxdrnzfl7w7wyx3kz"
+    let parsedAddress  = try zcash_address.parse(testSaplingAddress)
+    // assert(parsedAddress.network == .test)
+    assert(true)
+} catch {
+    fatalError("Invalid Address when it should have been valid")
+}
 
